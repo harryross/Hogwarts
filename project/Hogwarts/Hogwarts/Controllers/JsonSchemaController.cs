d76e07ab1da4d6d8657dba4418d923e8cf2914bd
Harry Ross <harry.ross@xero.com>
2015-04-09 10:04:42 +1200

    Json controller

---
+++ project/Hogwarts/Hogwarts/Controllers/JsonSchemaController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace Hogwarts.Controllers
+{
+    public class JsonSchemaController : ApiController
+    {
+        [HttpPost]
+        [Route("jsonSchema")]
+        public IHttpActionResult CreateSchema(string json)
+        {
+            return Ok();
+        }
+    }
+}
