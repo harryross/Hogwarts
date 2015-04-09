c719461f08c08d70de87e72b56d46552d1bd20c5
SeanCombs <drgasson@gmail.com>
2015-04-09 17:20:30 +1200

    Check json value type

---
+++ project/Hogwarts.Api/Helpers/JsonProperty.cs
@@ -0,0 +1,63 @@
+﻿namespace Hogwarts.Api.Helpers
+{
+	public class JsonProperty
+	{
+		public static string GetType(string value)
+		{
+			var temp = value.Trim();
+
+			// string
+			var isString = (temp[0] == '\'') && (temp[temp.Length - 1] == '\'');
+			if (isString)
+			{
+				return JsonType.String.ToString().ToLower();
+			}
+				
+			// null
+			if (temp == "null")
+			{
+				return JsonType.Null.ToString().ToLower();
+			}
+
+			// boolean
+			if (temp == "true" || temp == "false")
+			{
+				return JsonType.Boolean.ToString().ToLower();
+			}
+
+			// integer
+			int intResult;
+			var isInteger = int.TryParse(temp, out intResult);
+			if (isInteger)
+			{
+				return JsonType.Integer.ToString().ToLower();
+			}
+
+			// number
+			float numResult;
+			var isNumber = float.TryParse(temp, out numResult);
+			if (isNumber)
+			{
+				return JsonType.Number.ToString().ToLower();
+			}
+
+			// array
+
+			// object
+
+			return JsonType.Unknown.ToString().ToLower();
+		}
+
+		private enum JsonType
+		{
+			Array,
+			Boolean,
+			Integer,
+			Number,
+			Null,
+			Object,
+			String,
+			Unknown,
+		}
+	}
+}
