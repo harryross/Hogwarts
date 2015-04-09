5b01631a5592d5697dcdeba3f5b26bd0a349bd74
harryross <h.ross@outlook.com>
2015-04-09 13:28:13 +1200

    WIP Generator

--- project/Hogwarts.Api/Command/ConvertFromObjectToSchema.cs
+++ project/Hogwarts.Api/Command/ConvertFromObjectToSchema.cs
@@ -5,10 +5,40 @@ namespace Hogwarts.Api.Command
 {
     public class ConvertFromObjectToSchema
     {
+        private static int index = 0;
+        private string _schemaBuilder;
         public string GetJsonSchema(JsonObject json)
         {
             var result = JsonSchema.Parse(json.JsonObjectBody);
-            return result.ToString();
+            var generator = new JsonSchemaResolver();
+            _schemaBuilder = "";
+            GenerateSchema(json.JsonObjectBody);
+            return _schemaBuilder;
+        }
+
+
+        private void GenerateSchema(string json)
+        {
+            int temp = 0;
+            string subs = "";
+            _schemaBuilder += "{\n\t";
+            index = json.IndexOf('\'', index);
+            temp = json.IndexOf('\'', index+1);
+            subs = json.Substring(index, temp - 1);
+            _schemaBuilder += "'" + subs + "': {\n'type': 'string',\n'required': true}";
+            if (json.IndexOf('\'', temp) > json.IndexOf('{', temp) && json.IndexOf('{', temp) > 0)
+            {
+                GenerateSchema(json);
+            }
+            {
+
+            }
+            _schemaBuilder += "}";
+        }
+
+        private void GenerateUpToComma(string json)
+        {
+            
         }
     }
 }
