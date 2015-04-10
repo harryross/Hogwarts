db67d73bfb710a0ccb583c9182cd6473f877054e
harryross <h.ross@outlook.com>
2015-04-10 13:10:59 +1200

    it works

--- project/Hogwarts.Api/Command/ConvertFromObjectToSchema.cs
+++ project/Hogwarts.Api/Command/ConvertFromObjectToSchema.cs
@@ -6,24 +6,18 @@ namespace Hogwarts.Api.Command
     public class ConvertFromObjectToSchema
     {
         private static int _index = 0;
+        private int _indent = 0;
         private string _schemaBuilder;
         public string GetJsonSchema(JsonObject json)
         {
             _schemaBuilder = "";
             _index = 0;
-            GenerateSchema(json.JsonObjectBody);
+            _indent = 0;
+            StartSchema(json.JsonObjectBody);
             return _schemaBuilder;
         }
 
-
-        private void GenerateSchema(string json)
-        {
-            _schemaBuilder += "{";
-            GetJsonSubObject(json);
-            _schemaBuilder += "}";
-        }
-
-        private void GetJsonSubObject(string json)
+        private void GetName(string json)
         {
             int temp = 0;
             string subs = "";
@@ -31,59 +25,71 @@ namespace Hogwarts.Api.Command
             temp = json.IndexOf('\'', _index + 1);
             subs = json.Substring(_index + 1, temp - 1 - _index);
             _schemaBuilder += "'" + subs + "':";
-            if (json.IndexOf('\'', temp+1) > json.IndexOf('{', temp) && json.IndexOf('{', temp) > 0)
-            {
-                _index = json.IndexOf('{', _index);
-                GenerateJsonObject(json);
-            }
-            else if ((json.IndexOf('\'', temp + 1) > json.IndexOf('[', temp) && json.IndexOf('[', temp) > 0))
-            {
-                _index = json.IndexOf('[', _index);
-                GenerateJsonArray(json);
-            }
-            else
-            {
-                _index = json.IndexOf('\'', _index+1);
-                _index = json.IndexOf('\'', _index+1);
-                _index = json.IndexOf('\'', _index+1);
-                _schemaBuilder += "{'type': 'string', 'required': true}";
-            }
-            if (IsThereAComma(json))
-            {
-                _schemaBuilder += ",";
-                _index = json.IndexOf(',', _index);
-                GetJsonSubObject(json);
-            }
-            else
-            {
-                //_index = json.IndexOf('}', _index)+1;
-            }
+            
         }
 
-        private bool IsThereAComma(string json)
-        {
-            int indexOfBracket = json.IndexOf('}', _index);
-            int indexOfComma = json.IndexOf(',', _index);
-            return indexOfBracket > indexOfComma;
-        }
-
-        private void GenerateJsonArray(string json)
-        {
-            _schemaBuilder += "'type': 'array',";
-            _schemaBuilder += "'items': [";
-            GenerateSchema(json);
-            _schemaBuilder += "]";
-        }
-
-        private void GenerateJsonObject(string json)
+        private void InitiateObject(string json)
         {
             _schemaBuilder += "{";
             _schemaBuilder += "'type': 'object',";
             _schemaBuilder += "'required': true,";
-            _schemaBuilder += "'properties': ";
-            _index = json.IndexOf('{', _index);
-            GenerateSchema(json);
-            _schemaBuilder += "}";
+            _schemaBuilder += "'properties': {";
+        }
+
+        private void StartSchema(string json)
+        {
+            _schemaBuilder += "{";
+            while (true)
+            {
+                if (json.IndexOf('\'', _index) < json.IndexOf('}', _index) && json.IndexOf('\'', _index) > 0)
+                {
+                    GetName(json);
+                    _index = json.IndexOf(':', _index);
+                }
+                if (json.IndexOf('{', _index) < json.IndexOf('\'', _index) && json.IndexOf('{', _index) > 0)
+                {
+                    InitiateObject(json);
+                    _index = json.IndexOf('{', _index);
+                } else if (json.IndexOf('}', _index) != _index && json.IndexOf('{', _index) != _index)
+                {
+                    GetObjectType(json);
+                    
+                }
+                if (json.IndexOf(',', _index) < json.IndexOf('}', _index) && json.IndexOf(',', _index) > 0 && json.IndexOf('{', _index) != _index)
+                {
+                    _schemaBuilder += ",";
+                    _index = json.IndexOf(',', _index);
+                }
+                else if (json.IndexOf('{', _index) != _index)
+                {
+                    _index = json.IndexOf('}', _index) + 1;
+                    if (_index >= json.Length)
+                    {
+                        _schemaBuilder += "}";
+                        break;
+                    } else
+                    {
+                        _schemaBuilder += "}}";
+                    }
+                    if (json.IndexOf(',', _index) == _index && json.IndexOf('}', _index - 1) == _index - 1)
+                    {
+                        _schemaBuilder += ",";
+                        _index++;
+                    }
+                }
+                
+                if (_index < 0 || _index >= json.Length)
+                {
+                    break;
+                }
+                
+            }
+            _schemaBuilder += "";
+        }
+
+        private void GetObjectType(string json)
+        {
+            _schemaBuilder += "{'type': 'string', 'required': true}";
         }
     }
 }
